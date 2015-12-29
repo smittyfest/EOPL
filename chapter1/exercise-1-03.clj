e08729f8ffcf578e8c2b3edd694cdaaf99967d1b
smittyfest <smittyfest@users.noreply.github.com>
2015-12-28 21:40:26 -0600

    Create exercise-1-03.clj

---
+++ chapter1/exercise-1-03.clj
@@ -0,0 +1,11 @@
+;; ------------------------------------------------------------------------------
+;; Exercise 1.3[*]
+;;
+;; the set T of natural numbers where:
+;;   1) 0 in T, and
+;;   2) if n in T, then n+3 in T, and
+;;   3) T != S
+;; could be equal to N = {0,1,2,3,...}
+;; since we are no longer constrained
+;; to the smallest set satisfying these
+;; properties.
