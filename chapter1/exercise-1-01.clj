4063fe45466c872a1dcb39a4b9080029f692e251
smittyfest <smittyfest@users.noreply.github.com>
2015-12-28 20:10:19 -0600

    Added exercise 1.1.3

--- chapter1/exercise-1-01.clj
+++ chapter1/exercise-1-01.clj
@@ -7,13 +7,13 @@
 ;; {3n+2 | n in N} = {2, 5, 8, 11, ...}
 ;;
 ;; top-down: a natural number n is in S if and only if
-;; 1) n = 2, or
-;; 2) n - 3 in S.
+;;   1) n=2, or
+;;   2) n-3 in S.
 ;;
 ;; bottom-up: define the set S to be the smallest set contained in N and
 ;; satisfying the following two properties:
-;; 1) 2 is in S, and
-;; 2) n + 3 is in S if n in S.
+;;   1) 2 in S, and
+;;   2) if n in S, then n+3 in S.
 ;;
 ;; rules of inference:
 ;;               n in S
@@ -26,21 +26,38 @@
 ;; {2n+3m+1 | n,m in N} = {1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, ...}
 ;;
 ;; top-down: a natural number n is in S if and only if
-;; 1) n = 1, or
-;; 2) n - 2 in S, or
-;; 3) n - 3 in S.
+;;   1) n=1, or
+;;   2) n-2 in S, or
+;;   3) n-3 in S.
 ;;
 ;; bottom-up: define the set S to be the smallest set contained in N and
 ;; satisfying the following three properties:
-;; 1) 1 is in S, and
-;; 2) n + 2 is in S if n in S, and
-;; 3) n + 3 is in S if n in S.
+;;   1) 1 in S, and
+;;   2) if n in S, then n+2 in S, and
+;;   3) if n in S, then n+3 in S.
 ;;
 ;; rules of inference:
-;; 
 ;;               n in S       n in S
 ;;    ------   ----------   ----------
 ;;    1 in S   (n+2) in S   (n+3) in S
 ;;
 ;; ------------------------------------------------------------------------------
 ;; Exercise 1.1.3
+;;
+;; {(n,2n+1) | n in N} = {(0,1), (1,3), (2,5), (3,7), (4,9), ...}
+;;
+;; top-down: a pair of natural numbers (n,m) is in S if and only if
+;;   1) (n,m)=(0,1), or
+;;   2) (n-1,m-2) in S.
+;;
+;; bottom-up: define the set S to be the smallest set contained in N and
+;; satisfying the following two properties:
+;;   1) (0,1) in S, and
+;;   2) if (n,m) in S, then (n+1,m+2) in S.
+;;
+;; rules of inference:
+;;                 (n,m) in S
+;;   ----------  --------------
+;;   (0,1) in S  (n+1,m+2) in S
+;;
+;; ------------------------------------------------------------------------------
