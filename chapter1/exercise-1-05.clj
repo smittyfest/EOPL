5eb32302c69a66d5ec3c0ec95a51a5e664f02a3b
smittyfest <smittyfest@users.noreply.github.com>
2015-12-30 11:29:14 -0600

    Create exercise-1-05.clj

---
+++ chapter1/exercise-1-05.clj
@@ -0,0 +1,22 @@
+;; ------------------------------------------------------------------------------
+;; Exercise 1.5[**]
+;;
+;; Prove that if expression e is in LcExp, then there are the same number
+;; of left and right parentheses in e.
+;;
+;; Proof: The proof is by structural induction on the size k of e,
+;; where k is the number of symbolic expressions in e.
+;; The induction hypothesis IH(k) is that any symbolic expression
+;; e having size <= k has a matching number of left and right parentheses.
+;;
+;; 1. LcExp ::= Identifier, where Identifier has no parentheses, so IH(0) holds trivially.
+;; 2. Let k be an integer such that IH(k) holds, that is, any symbolic expression
+;;    with <= k sub-expressions actually has matching left and right parentheses.
+;;    We need to show that IH(k+1) holds as well: that any s-expression with <= k+1
+;;    sub-expressions has a matching number of left and right parentheses. If e has
+;;    <= k+1 sub-expressions there are exactly three possibilities according to the
+;;    definition of LcExp:
+;;
+;;    a) e could be of the form 'Identifier', where identifiers have no parentheses,
+;;       and thus has a matching number (0) of left and right parentheses.
+;;    b) e could be of the form (λ (Identifier) LcExp)
