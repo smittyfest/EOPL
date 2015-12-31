5d6c9ac7a9037e39354010b4616d5d36d7116a9e
smittyfest <smittyfest@users.noreply.github.com>
2015-12-31 11:35:31 -0600

    Update exercise-1-06.clj

--- chapter1/exercise-1-06.clj
+++ chapter1/exercise-1-06.clj
@@ -9,13 +9,13 @@
 (defn nth-elem
   {:doc "Return nth-element of the list"}
   [lst n]
-  (loop [count n tmp lst]
-    (if (zero? count) 
+  (loop [cnt n tmp lst]
+    (if (zero? cnt) 
     (first tmp) 
-    (recur (dec count) (rest tmp)))))
+    (recur (dec cnt) (rest tmp)))))
 
 ;;
 ;; run some unit-tests
 ;;
-(is (nth-elem '(a b c d e) 3) d)
-;; ==> d
+(is (= (nth-elem '(a b c d e) 3) 'd))
+
