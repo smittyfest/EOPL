8825073bef351eb06ba23ef25267f2fec571321d
smittyfest <smittyfest@users.noreply.github.com>
2016-01-01 12:42:32 -0600

    Update exercise-1-08.clj

--- chapter1/exercise-1-08.clj
+++ chapter1/exercise-1-08.clj
@@ -1,3 +1,10 @@
 ;; ------------------------------------------------------------------------------
 ;; Exercise 1.8[*]
 ;;
+(ns eopl.ch01 (:use clojure.test))
+(defn remove-first
+  {:doc "Return first-occurrence of s from the list xs"}
+  [xs s]
+  (if (empty? xs)
+  '()
+  (loop [tmp xs]
