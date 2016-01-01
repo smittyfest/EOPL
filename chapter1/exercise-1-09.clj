f68bc39e2ad5725d46f531a4804e72391baea170
smittyfest <smittyfest@users.noreply.github.com>
2016-01-01 12:44:18 -0600

    Update exercise-1-09.clj

--- chapter1/exercise-1-09.clj
+++ chapter1/exercise-1-09.clj
@@ -1,3 +1,7 @@
 ;; ------------------------------------------------------------------------------
 ;; Exercise 1.9[**]
 ;;
+(ns eopl.ch01 (:use clojure.test))
+(defn remove
+  {:doc "Remove all occurrences of s from the list xs"}
+  [xs s]
