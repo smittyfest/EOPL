0e126283e4b34136283d5f55c0bb8546020383be
smittyfest <smittyfest@users.noreply.github.com>
2016-01-01 17:11:46 -0600

    Update exercise-1-08.clj

--- chapter1/exercise-1-08.clj
+++ chapter1/exercise-1-08.clj
@@ -10,6 +10,15 @@
     (if (= s (first xs))
       (rest xs)
         (cons (first xs) (remove-first (rest xs) s)))))
+      
+(defn remove-up-to
+  {:doc "Remove all elements up to and including the first occurrence of s from the list xs"}
+  [xs s]
+  (if (empty? xs)
+    ()
+    (if (= s (first xs))
+      (rest xs)
+        (remove-up-to (rest xs) s))))
 ;;
 ;; run some unit-tests
 ;;
@@ -17,3 +26,8 @@
 (is (= (remove-first '(a b c) 'e) '(a b c)))
 (is (= (remove-first () 'a) ()))
 (is (= (remove-first '(a b c a b c) 'c) '(a b a b c)))
+(is (= (remove-up-to '(a b c) 'a) '(b c)))
+(is (= (remove-up-to '(a b c) 'e) ()))
+(is (= (remove-up-to () 'a) ()))
+(is (= (remove-up-to '(a b c a b c) 'c) '(a b c)))
+(is (= (remove-up-to '(q r s t u v w x y z) 'y) '(z)))
