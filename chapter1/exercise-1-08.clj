5085c4e06acca6b9fc55de35f1914d5375153f84
smittyfest <smittyfest@users.noreply.github.com>
2016-01-01 13:01:11 -0600

    Update exercise-1-08.clj

--- chapter1/exercise-1-08.clj
+++ chapter1/exercise-1-08.clj
@@ -3,8 +3,16 @@
 ;;
 (ns eopl.ch01 (:use clojure.test))
 (defn remove-first
-  {:doc "Remove the first occurrence of s from the list xs"}
+  {:doc "Remove first occurrence of s from the list xs"}
   [xs s]
   (if (empty? xs)
   '()
   (loop [tmp xs]
+    (if (= (s (first tmp)))
+      (rest tmp)
+    (recur (cons (first tmp) (remove-first s (rest tmp))))))))
+
+;;
+;; run some unit-tests
+;;
+(is (= (remove-first '(a b c) 'a) '(b c)))
