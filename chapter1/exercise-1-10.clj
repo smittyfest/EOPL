8c956704890fdffb1db7c6ef493aa2597ab48050
smittyfest <smittyfest@users.noreply.github.com>
2016-01-02 10:53:03 -0600

    Update exercise-1-10.clj

--- chapter1/exercise-1-10.clj
+++ chapter1/exercise-1-10.clj
@@ -1,3 +1,5 @@
 ;; ------------------------------------------------------------------------------
 ;; Exercise 1.10[*]
 ;;
+;; "or" can also mean "exclusive-or", meaning "either x, or y, but not both".
+;; exclusive-or is typically represented by the shorthand "xor" and the symbol '^'.
