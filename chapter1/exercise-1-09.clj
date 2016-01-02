57c0109e328832b052578b68742c234f6069449b
smittyfest <smittyfest@users.noreply.github.com>
2016-01-02 10:42:09 -0600

    Update exercise-1-09.clj

--- chapter1/exercise-1-09.clj
+++ chapter1/exercise-1-09.clj
@@ -2,6 +2,19 @@
 ;; Exercise 1.9[**]
 ;;
 (ns eopl.ch01 (:use clojure.test))
-(defn remove
+(defn remove-all
   {:doc "Remove all occurrences of s from the list xs"}
   [xs s]
+  (if (empty? xs)
+    ()
+    (if (= s (first xs)) (remove-all (rest xs) s)
+  (cons (first xs) (remove-all (rest xs) s)))))
+;;
+;; run some unit-tests
+;;
+(is (= (remove-all () 'a) ()))
+(is (= (remove-all '(a b) 'a) '(b)))
+(is (= (remove-all '(a b c) 'b) '(a c)))
+(is (= (remove-all '(a b c b d b e f) 'b) '(a c d e f)))
+(is (= (remove-all '(a b c b d b e f b) 'b) '(a c d e f)))
+(is (= (remove-all '(a b c) 'e) '(a b c)))
