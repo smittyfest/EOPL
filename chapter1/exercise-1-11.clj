2b1d96fcf1aa18ed9e7273baea97ae6687ec110c
smittyfest <smittyfest@users.noreply.github.com>
2016-01-05 19:26:47 -0600

    Update exercise-1-11.clj

--- chapter1/exercise-1-11.clj
+++ chapter1/exercise-1-11.clj
@@ -8,8 +8,20 @@
 ;;
 (ns eopl.ch01 (:use clojure.test))
 (defn subst
-  {:doc "Return a new List with each occurrence of 'old' replaced by 'new'}
+  {:doc "Return a new S-List with each occurrence of 'old' replaced by 'new'}
   [old new lst]
   (if (empty? lst)
       ()
+      (cons
+        (subst-in-s-exp old new (first lst))
+        (subst old new (rest lst)))))
+      
+(defn subst-in-s-exp
+  {:doc "Return a new S-Exp with each occurrence of 'old' replaced by 'new'}
+  [old new exp]
+  (if (symbol? exp
+  ;;
+  ;; run some unit-tests
+  ;;
+  (is (= (subst 'b 'a '()) ()))
       
