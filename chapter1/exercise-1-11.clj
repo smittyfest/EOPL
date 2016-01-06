f27700ad747e62135039a2b0c5ef3a19cd178d0a
smittyfest <smittyfest@users.noreply.github.com>
2016-01-05 19:04:57 -0600

    Update exercise-1-11.clj

--- chapter1/exercise-1-11.clj
+++ chapter1/exercise-1-11.clj
@@ -2,3 +2,7 @@
 ;; ------------------------------------------------------------------------------
 ;; Exercise 1.11[*]
 ;;
+(ns eopl.ch01 (:use clojure.test))
+(defn subst
+  {:doc "Return a new List with each occurrence of 'old' replaced by 'new'}
+  [old new lst]
