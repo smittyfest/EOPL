e48da71b8885728c486bd785e7ff8fca9df48f1c
smittyfest <smittyfest@users.noreply.github.com>
2016-01-07 15:03:41 -0600

    Update exercise-1-14.clj

--- chapter1/exercise-1-14.clj
+++ chapter1/exercise-1-14.clj
@@ -21,5 +21,25 @@
       (list n (first lst))
       (number-elements-from (rest lst) (+ n 1)))))
 
+(defn partial-vector-sum
+  {:doc "sum the elements in a slice of a vector of Ints"}
+  [vec n]
+  (if (zero? n)
+      (nth vec 0)
+      (+ (nth vec n) (partial-vector-sum vec (- n 1)))))
+
+(defn vector-sum
+  {:doc "sum all of the elements in a vector of Ints"}
+  [vec]
+  (let [n (count vec)]
+  (if (zero? n)
+    0
+    (partial-vector-sum vec (- n 1)))))
+;;
+;; unit-tests
+;;
 (is (= (number-elements ()) ()))
 (is (= (number-elements '('a 'b 'c)) '((0 'a) (1 'b) (2 'c))))
+(is (= (vector-sum []) 0))
+(is (= (vector-sum [1 2 3]) 6))
+(is (= (vector-sum [25 25 25 25]) 100))
