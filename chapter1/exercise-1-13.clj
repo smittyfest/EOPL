f725ced302aa4d5c96e96ceacb673291fa5a4afa
smittyfest <smittyfest@users.noreply.github.com>
2016-01-07 11:35:38 -0600

    Update exercise-1-13.clj

--- chapter1/exercise-1-13.clj
+++ chapter1/exercise-1-13.clj
@@ -1,3 +1,16 @@
-;; ------------------------------------------------------------------------------
-;; Exercise 1.13[**]
+
+;; ==============================================================================
 ;;
+;; @@@@@@@@@@@@@@@@@@@@@
+;; @ Exercise 1.13[**] @
+;; @@@@@@@@@@@@@@@@@@@@@
+;;
+;; In our example, we began by eliminating the Kleene Star in the
+;; grammar for S-List. Write subst following the original grammar by using map.
+;;
+;; ~ Original Grammar ~
+;; S-List ::= ({S-Exp}*)
+;;  S-Exp ::= Symbol | S-List
+;;
+(ns eopl.ch01 (:use clojure.test))
+(defn subst
