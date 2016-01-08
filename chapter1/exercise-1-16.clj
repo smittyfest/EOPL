c66cddaac4e3f90fbbf3c4a9021f41aa404f8b2c
smittyfest <smittyfest@users.noreply.github.com>
2016-01-08 11:26:04 -0600

    Update exercise-1-16.clj

--- chapter1/exercise-1-16.clj
+++ chapter1/exercise-1-16.clj
@@ -8,3 +8,13 @@
 (defn invert
   {:doc "Reverses the contents of each tuple in a list of tuples"}
   [xs]
+  (if (empty? xs)
+    ()
+    (cons (reverse (first xs)) (invert (rest xs)))))
+;;
+;; unit-tests
+;;
+(is (= (invert ()) ()))
+(is (= (invert '((b a))) '((a b))))
+(is (= (invert '((b a) (d c) (f e) (h g) (j i))) '((a b) (c d) (e f) (g h) (i j))))
+(is (= (invert '((a 1) (a 2) (1 b) (2 b))) '((1 a) (2 a) (b 1) (b 2))))
