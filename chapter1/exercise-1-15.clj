a49e27287ce2657cc00737abb563769e3d8ae676
smittyfest <smittyfest@users.noreply.github.com>
2016-01-08 11:08:21 -0600

    Update exercise-1-15.clj

--- chapter1/exercise-1-15.clj
+++ chapter1/exercise-1-15.clj
@@ -4,3 +4,15 @@
 ;; @ Exercise 1.15[*] @
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
+(ns eopl.ch01 (:use clojure.test))
+
+(defn duple
+  {:doc "Returns a list containing n copies of x"}
+  [x n]
+  (if (zero? n)
+    ()
+    (cons x (duple x (- n 1)))))
+
+(is (= (duple 5 0) ()))
+(is (= (duple 5 3) '(5 5 5)))
+(is (= (duple 'a 10) '(a a a a a a a a a a)))
