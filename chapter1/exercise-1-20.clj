86b0efc9e753c1c333eb01353d5651d7f7d45321
smittyfest <smittyfest@users.noreply.github.com>
2016-01-10 16:15:03 -0600

    Update exercise-1-20.clj

--- chapter1/exercise-1-20.clj
+++ chapter1/exercise-1-20.clj
@@ -5,3 +5,9 @@
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
 (ns eopl.ch01 (:use clojure.test))
+(declare counter)
+(defn count-occurrences
+  {:doc "Return the number of occurrences of x in xs"}
+  [x xs]
+  (if (empty? xs) 0
+    (counter 0 x xs)))
