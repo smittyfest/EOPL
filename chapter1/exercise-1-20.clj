0427dae33ebb0512103b47b93e94854d6b61e0a5
smittyfest <smittyfest@users.noreply.github.com>
2016-01-10 16:19:25 -0600

    Update exercise-1-20.clj

--- chapter1/exercise-1-20.clj
+++ chapter1/exercise-1-20.clj
@@ -11,3 +11,23 @@
   [x xs]
   (if (empty? xs) 0
     (counter 0 x xs)))
+
+(defn- counter
+  {:doc "Accumulates count totals for x in xs"}
+  [accu x xs]
+  (cond
+    (empty? xs) accu
+    (symbol? (first xs))
+      (if (= x (first xs))
+        (counter (inc accu) x (rest xs))
+        (counter accu x (rest xs)))
+    :else (counter accu x (first xs))))
+;;
+;; unit-tests
+;;
+(is (= (count-occurrences 'a ()) 0))
+(is (= (count-occurrences 'a '(a b)) 1))
+(is (= (count-occurrences 'a '(a a a a a)) 5))
+(is (= (count-occurrences 'a '((f a) y (((a z) a)))) 3))
+(is (= (count-occurrences 'a '((f a) y (((a z) () a)))) 3))
+(is (= (count-occurrences 'a '((f x) y (((x z) x)))) 0))
