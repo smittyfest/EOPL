59ff73df27a998bf69355fae87fa8c1a6cd647b0
smittyfest <smittyfest@users.noreply.github.com>
2016-01-11 12:29:41 -0600

    Update exercise-1-21.clj

--- chapter1/exercise-1-21.clj
+++ chapter1/exercise-1-21.clj
@@ -8,3 +8,9 @@
 (defn product
   {:doc "Returns the Cartesian Product of two Lists"}
   [xs ys]
+  (for [x xs y ys] (list x y)))
+;;
+;; unit-tests
+;;
+(is (= (product '(a b c) '(x y)) '((a x) (a y) (b x) (b y) (c x) (c y))))
+(is (= (product '(a b c) '(x y z)) '((a x) (a y) (a z) (b x) (b y) (b z) (c x) (c y) (c z))))
