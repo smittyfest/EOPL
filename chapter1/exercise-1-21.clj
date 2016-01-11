1aa0b386ea53b8cf9f38209d6b449124a85988cb
smittyfest <smittyfest@users.noreply.github.com>
2016-01-11 12:32:28 -0600

    Update exercise-1-21.clj

--- chapter1/exercise-1-21.clj
+++ chapter1/exercise-1-21.clj
@@ -12,5 +12,8 @@
 ;;
 ;; unit-tests
 ;;
+(is (= (product '(a b) ()) ()))
+(is (= (product () '(a b)) ()))
+(is (= (product '(a) '(b)) '((a b))))
 (is (= (product '(a b c) '(x y)) '((a x) (a y) (b x) (b y) (c x) (c y))))
 (is (= (product '(a b c) '(x y z)) '((a x) (a y) (a z) (b x) (b y) (b z) (c x) (c y) (c z))))
