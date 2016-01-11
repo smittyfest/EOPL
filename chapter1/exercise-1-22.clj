3f3b590465e19b5f058efd317b2bb273fda595ac
smittyfest <smittyfest@users.noreply.github.com>
2016-01-11 12:49:34 -0600

    Update exercise-1-22.clj

--- chapter1/exercise-1-22.clj
+++ chapter1/exercise-1-22.clj
@@ -4,3 +4,16 @@
 ;; @ Exercise 1.22[**] @
 ;; @@@@@@@@@@@@@@@@@@@@@
 ;;
+(ns eopl.ch01 (:use clojure.test))
+(defn filter-in
+  {:doc "Returns a list of elements satisfying a predicate"}
+  [pred xs]
+  (if (empty? xs) ()
+    (if (pred (first xs))
+      (cons (first xs) (filter-in pred (rest xs)))
+      (filter-in pred (rest xs)))))
+;;
+;; unit-tests
+;;
+(is (= (filter-in number? '(a 2 (1 3) b 7)) '(2 7)))
+(is (= (filter-in symbol? '(a (b c) 17 f)) '(a f)))
