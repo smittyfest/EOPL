dd9230f2c7e46691e13676fb5c62ae171b53b406
smittyfest <smittyfest@users.noreply.github.com>
2016-01-14 11:53:11 -0600

    Update exercise-1-26.clj

--- chapter1/exercise-1-26.clj
+++ chapter1/exercise-1-26.clj
@@ -9,8 +9,10 @@
   {:doc "Removes a pair of parentheses from each top-level element of a List"}
   [xs]
   (if (empty? xs) ()
-    (if (symbol? (first xs)) (cons (first xs) (up (rest xs)))
-      (up (first xs)))))
+    (if (list? (first xs))
+      (concat (first xs) (up (rest xs)))
+      (cons (first xs) (up (rest xs))))))
 ;; unit-tests
 (is (= (up '((1 2) (3 4))) '(1 2 3 4)))
 (is (= (up '((x (y)) z)) '(x (y) z)))
+(is (= (up '((a) (b) (c) (d))) '(a b c d)))
