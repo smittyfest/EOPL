20e00dea7aa88326dfc3d27053bc5f16add901e0
smittyfest <smittyfest@users.noreply.github.com>
2016-01-14 12:25:37 -0600

    Update exercise-1-28.clj

--- chapter1/exercise-1-28.clj
+++ chapter1/exercise-1-28.clj
@@ -4,3 +4,16 @@
 ;; @ Exercise 1.28[**] @
 ;; @@@@@@@@@@@@@@@@@@@@@
 ;;
+(ns eopl.ch01 (:use clojure.test))
+(defn merge
+  {:doc "given two lists of integers, returns a single sorted list"}
+  [xs ys]
+  (cond
+    (empty? xs) ys
+    (empty? ys) xs
+    (<= (first xs) (first ys)) (cons (first xs) (merge (rest xs) ys))
+    :else (cons (first ys) (merge xs (rest ys)))))
+;;
+;; unit-tests
+;;
+(is (= (merge '(1 4) '(1 2 8)) '(1 1 2 4 8)))
