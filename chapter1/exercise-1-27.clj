708e3069ba7da800ab581dc59b0d51d0b5a3943c
smittyfest <smittyfest@users.noreply.github.com>
2016-01-14 12:04:51 -0600

    Update exercise-1-27.clj

--- chapter1/exercise-1-27.clj
+++ chapter1/exercise-1-27.clj
@@ -4,3 +4,13 @@
 ;; @ Exercise 1.27[**] @
 ;; @@@@@@@@@@@@@@@@@@@@@
 ;;
+(ns eopl.ch01 (:use clojure.test))
+(defn flatten
+  {:doc "takes an arbitrarily-nested list and returns a flat list"}
+  [xs]
+  (if (empty? xs) ()
+    (if (list? (first xs))
+      (concat (flatten (first xs)) (flatten (rest xs)))
+      (cons (first xs) (flatten (rest xs))))))
+;; unit-tests
+(is (= (flatten '(a b c)) '(a b c)))
