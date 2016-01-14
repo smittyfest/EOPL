5eef68b6a4216f7264281dc0757ea4f7447444d9
smittyfest <smittyfest@users.noreply.github.com>
2016-01-14 12:08:03 -0600

    Update exercise-1-27.clj

--- chapter1/exercise-1-27.clj
+++ chapter1/exercise-1-27.clj
@@ -12,5 +12,10 @@
     (if (list? (first xs))
       (concat (flatten (first xs)) (flatten (rest xs)))
       (cons (first xs) (flatten (rest xs))))))
+;;
 ;; unit-tests
+;;
 (is (= (flatten '(a b c)) '(a b c)))
+(is (= (flatten '((a) () (b ()) () (c))) '(a b c)))
+(is (= (flatten '((a b) c (((d)) e))) '(a b c d e)))
+(is (= (flatten '(a b (() (c)))) '(a b c)))
