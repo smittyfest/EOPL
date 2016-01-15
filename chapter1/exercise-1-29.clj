5145ebff2db8c0bd2478b61648ee3d808d568a35
smittyfest <smittyfest@users.noreply.github.com>
2016-01-15 10:33:52 -0600

    Update exercise-1-29.clj

--- chapter1/exercise-1-29.clj
+++ chapter1/exercise-1-29.clj
@@ -21,3 +21,8 @@
     (empty? ys) xs
     (<= (first xs) (first ys)) (cons (first xs) (merge (rest xs) ys))
     :else (cons (first ys) (merge xs (rest ys)))))
+;;
+;; unit-tests
+;;
+(is (= (sort '(8 2 5 2 3)) '(2 2 3 5 8)))
+(is (= (sort '(9 8 7 6 5 4 3 2 1 0)) '(0 1 2 3 4 5 6 7 8 9)))
