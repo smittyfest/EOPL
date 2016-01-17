25cef031a3d0354dbd5a57f63b094ddf0e1f5ed3
smittyfest <smittyfest@users.noreply.github.com>
2016-01-17 15:28:26 -0600

    Update exercise-1-35.clj

--- chapter1/exercise-1-35.clj
+++ chapter1/exercise-1-35.clj
@@ -40,3 +40,18 @@
   {:doc "returns a bintree with the contents of the leaves numbered starting from zero"}
   [tree]
   (accumulate-leaves tree 0))
+
+(defn accumulate-leaves
+  {:doc "iterates over the nodes of a tree, building a new tree with the accumulated values"}
+  [tree accu]
+  (if (leaf? tree) (cons (inc accu) (list (leaf accu)))
+  (let [[left-accu & left-tree] (accumulate-leaves (lson tree) accu)
+        [right-accu & right-tree] (accumulate-leaves (rson tree) left-accu)]
+    (cons right-accu (interior-node (contents-of tree) left-tree right-tree)))))
+  ;;
+  ;; unit-tests
+  ;;
+  (is (= '(foo (bar (0) (1))(baz (2) (quxx (3) (4)))))
+          (number-leaves (interior-node 'foo (interior-node 'bar (leaf 26) (leaf 12))
+                                             (interior-node 'baz (leaf 11)
+                                             (interior-node 'quux (leaf 117) (leaf 14))))))
