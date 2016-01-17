d3e4c7fdade48f3b158fbb7860def3fb3197731c
smittyfest <smittyfest@users.noreply.github.com>
2016-01-17 16:01:14 -0600

    Update exercise-1-36.clj

--- chapter1/exercise-1-36.clj
+++ chapter1/exercise-1-36.clj
@@ -5,3 +5,11 @@
 ;; @@@@@@@@@@@@@@@@@@@@@@
 ;;
 (ns eopl.ch01 (:use clojure.test))
+
+(declare g)
+
+(defn number-elements
+  {:doc "takes a list (v0 v1 ..) and returns a list ((0 v0) (1 v1) ..)"}
+  [xs]
+  (if (empty? xs) ()
+    (g (list 0 (first xs)) (number-elements (rest xs)))))
