bdb0854d7263060d5316ba9eb643c902fd45b399
smittyfest <smittyfest@users.noreply.github.com>
2016-01-17 16:03:11 -0600

    Update exercise-1-36.clj

--- chapter1/exercise-1-36.clj
+++ chapter1/exercise-1-36.clj
@@ -13,3 +13,8 @@
   [xs]
   (if (empty? xs) ()
     (g (list 0 (first xs)) (number-elements (rest xs)))))
+
+(defn g
+  {:doc "handles accumulating count and adding it to list"}
+  [x xs]
+  (cons x (map (fn [i] (cons (+ (first i) 1) (rest i))) xs)))
