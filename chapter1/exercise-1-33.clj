47553b0f0aa7fa29887cb6cbc1f36dc602eb1fd4
smittyfest <smittyfest@users.noreply.github.com>
2016-01-18 10:36:02 -0600

    Update exercise-1-33.clj

--- chapter1/exercise-1-33.clj
+++ chapter1/exercise-1-33.clj
@@ -54,11 +54,7 @@
 ;;
 (is (= (mark-leaves-with-red-depth
          (interior-node 'red
-         (interior-node 'bar
-           (leaf 26)
-           (leaf 12))
-             (interior-node 'red
-               (leaf 11)
-               (interior-node 'quux
-                 (leaf 117)
-                 (leaf 14))))) '(red (bar 1 1) (red 2 (quux 2 2)))))
+         (interior-node 'bar (leaf 26) (leaf 12))
+         (interior-node 'red (leaf 11)
+         (interior-node 'quux (leaf 117) (leaf 14)))))
+           '(red (bar 1 1) (red 2 (quux 2 2)))))
