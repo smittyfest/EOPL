f0ed9363227d459edc37a4b583d3fd96750d07bb
smittyfest <smittyfest@users.noreply.github.com>
2016-01-19 11:43:12 -0600

    Update exercise-2-01.clj

--- chapter2/exercise-2-01.clj
+++ chapter2/exercise-2-01.clj
@@ -6,11 +6,11 @@
 ;;
 (ns eopl.ch02 (:use clojure.test))
 
-(defn zero
-  {:doc "returns bigint representation of zero"}
-  ())
+(def BASE 10)
 
-(defn zero?
+(def zero ()) ;; returns bigint representation of zero
+
+(defn is-zero?
   {:doc "returns true if n is equal to the bigint representation of zero"}
   [n]
   (empty? n))
@@ -18,7 +18,20 @@
 (defn successor
   {:doc "returns the successor to n in bigint representation"}
   [n]
+  (cond
+    (is-zero? n) '(1)
+    (= (inc (first n)) BASE) (cons 0 (successor (rest n)))
+    :else (cons (+ (first n) 1) (rest n))))
 
 (defn predecessor
   {:doc "returns the predecessor to n in bigint representation"}
   [n]
+  (cond
+    (zero? (first n)) (cons (- BASE 1) (predecessor (rest n)))
+    (and (= (first n) 1) (is-zero? (rest n))) ()
+    :else (cons (- (first n) 1) (rest n))))
+;;
+;; unit-tests
+;;
+(is (= (is-zero? ()) true))
+(is (= (successor ()) '(1)))
