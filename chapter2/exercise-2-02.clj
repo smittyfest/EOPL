5f8703ad91ed242feeeb0d6cb76caf6cd2a81d1a
smittyfest <smittyfest@users.noreply.github.com>
2016-01-20 11:06:38 -0600

    Update exercise-2-02.clj

--- chapter2/exercise-2-02.clj
+++ chapter2/exercise-2-02.clj
@@ -4,3 +4,16 @@
 ;; @ Exercise 2.2[**] @
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
+(ns eopl.ch02 (:use clojure.test))
+
+;; unary representation
+(def zero ())
+(defn is-zero? [n] (empty? n))
+(defn succ [n] (cons true n))
+(defn pred [n] (rest n))
+
+;; Scheme representation
+(def zero 0)
+(defn is-zero? [n] (zero? n))
+(defn succ [n] (+ n 1))
+(defn pred [n] (- n 1))
