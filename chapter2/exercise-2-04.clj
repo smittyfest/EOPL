0edd9e62803ea267a4709e4ab99dbba7e4c270e5
smittyfest <smittyfest@users.noreply.github.com>
2016-01-30 11:10:03 -0600

    Update exercise-2-04.clj

--- chapter2/exercise-2-04.clj
+++ chapter2/exercise-2-04.clj
@@ -4,3 +4,11 @@
 ;; @ Exercise 2.4[**] @
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
+;; (empty s)     = ()
+;; (push [f] s)  = (f . s)
+;; (pop s)       = (cdr s)
+;; (top s)       = (car s)
+;; (empty? s)    = {#t if s = (), #f otherwise}
+;;
+;; constructors: empty, push, pop
+;; observers: top, empty?
