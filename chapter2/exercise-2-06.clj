65f34b3929947d88f9258080cee5eb93fa270478
smittyfest <smittyfest@users.noreply.github.com>
2016-02-05 12:31:51 -0600

    Update exercise-2-06.clj

--- chapter2/exercise-2-06.clj
+++ chapter2/exercise-2-06.clj
@@ -5,4 +5,4 @@
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
 ;; Map, Set, and Tree representations of environments.
-;; Insertion-order is maintained with an auxilliary list of keys.
+;; Insertion-order is maintained with an auxiliary list of keys.
