af14ae92a42d27cd93556d9ab88e7ae5c4a7ebb4
smittyfest <smittyfest@users.noreply.github.com>
2016-02-08 19:29:40 -0600

    Update exercise-2-05.clj

--- chapter2/exercise-2-05.clj
+++ chapter2/exercise-2-05.clj
@@ -5,6 +5,7 @@
 ;; @@@@@@@@@@@@@@@@@@@
 ;;
 ;; association-list implementation of environments
+(ns eopl.ch02 (:use clojure.test))
 
 (defn empty-env
   {:doc "Representation of an empty environment"}
