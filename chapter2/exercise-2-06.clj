f981d2f20b2dad33fd1d73d7d301f2bd7fcb6872
smittyfest <smittyfest@users.noreply.github.com>
2016-02-13 10:46:36 -0600

    Update exercise-2-06.clj

--- chapter2/exercise-2-06.clj
+++ chapter2/exercise-2-06.clj
@@ -5,5 +5,20 @@
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
 ;; Map, Set, and Tree representations of environments.
-;; Insertion-order is maintained with an auxiliary list of keys.
+;;
 (ns eopl.ch02 (:use clojure.test))
+
+(defn empty-env
+  {:doc "Map Representation of an empty environment"}
+  []
+   {})
+
+(defn extend-env
+  {:doc ""}
+  [var val env]
+  (assoc env var val))
+
+(defn apply-env
+  {:doc ""}
+  [search-var env]
+  (get env search-var))
