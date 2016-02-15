0fe6f05103ffed2e7fa52a43482d5ed372d410d6
smittyfest <smittyfest@users.noreply.github.com>
2016-02-15 10:00:47 -0600

    Update exercise-2-07.clj

--- chapter2/exercise-2-07.clj
+++ chapter2/exercise-2-07.clj
@@ -4,4 +4,23 @@
 ;; @ Exercise 2.7[*] @
 ;; @@@@@@@@@@@@@@@@@@@
 ;;
+;; Print out error msgs for missing keys or poorly-defined environments
+;;
 (ns eopl.ch02 (:use clojure.test))
+
+(defn apply-env
+  {:doc "Retrieve a value for a given variable in an environment"}
+  [search-var env]
+  (cond
+    (= env []) (println "No binding found for " + search-var)
+    (sequential? env)
+      (let [[var val] (peek env)]
+        (if (= search-var var) val
+        (recur search-var (pop env))))
+    :else (println "No binding found for environment " + env)))
+;;
+;; unit-tests
+;;
+(is (= (apply-env :a '[[:b \b] [:a \a]]) \a))
+(apply-env :a '[[:b \b]])
+(apply-env :a '{:a \a})
