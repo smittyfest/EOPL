0f62482fccfd9ddb9d1337fcc4f6d2b7f0f5ebba
smittyfest <smittyfest@users.noreply.github.com>
2016-02-20 12:47:56 -0600

    Update exercise-2-12.clj

--- chapter2/exercise-2-12.clj
+++ chapter2/exercise-2-12.clj
@@ -1,8 +1,49 @@
-
 ;; ==============================================================================
 ;;
 ;; @@@@@@@@@@@@@@@@@@@@
 ;; @ Exercise 2.12[*] @
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
+;; (empty s)     = ()
+;; (push [f] s)  = (f . s)
+;; (pop s)       = (cdr s)
+;; (top s)       = (car s)
+;; (empty? s)    = {#t if s = (), #f otherwise}
+;;
+;; constructors: empty, push, pop
+;; observers: top, empty?
+;;
 (ns eopl.ch02 (:use clojure.test))
+
+(defn empty
+  {:doc ""}
+  []
+  (fn [x]
+    (cond
+    (= x 'empty?) true
+    :else (throw (IllegalArgumentException.)))))
+
+(defn push
+  {:doc ""}
+  [stack x]
+  (fn [y]
+    (cond
+      (= y 'empty?) false
+      (= y 'top) x
+      (= y 'pop) stack
+      :else (throw (IllegalArgumentException.)))))
+
+(defn empty?
+  {:doc ""}
+  [stack]
+  (stack 'empty?))
+
+(defn pop
+  {:doc ""}
+  [stack]
+  (stack 'pop))
+
+(defn top
+  {:doc ""}
+  [stack]
+  (stack 'top))
