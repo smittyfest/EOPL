66db6139a7816e6310e1a8c1ff99edbb8194cbfb
smittyfest <smittyfest@users.noreply.github.com>
2016-02-25 13:31:15 -0600

    Update exercise-2-14.clj

--- chapter2/exercise-2-14.clj
+++ chapter2/exercise-2-14.clj
@@ -5,3 +5,29 @@
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
 (ns eopl.ch02 (:use clojure.test))
+
+(defn empty-env
+  []
+  [(fn [] true)
+   (fn [search-var] (throw (IllegalStateException.)))])
+
+(defn apply-env
+  [search-var env]
+  (search-var (first env)))
+
+(defn extend-env
+  [var val env]
+  [(fn [] false)
+   (fn [search-var]
+     (if (= var search-var) val
+       (apply-env search-var env)))])
+
+(defn empty-env?
+  [env]
+  ((second env)))
+
+(defn has-binding? [search-var env]
+  (search-var (second env)))
+;;
+;; unit-tests
+;;
