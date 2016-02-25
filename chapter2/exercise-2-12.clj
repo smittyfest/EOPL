cbb1a65e5b68115bf26735658c51413068235b7a
smittyfest <smittyfest@users.noreply.github.com>
2016-02-25 13:15:33 -0600

    Update exercise-2-12.clj

--- chapter2/exercise-2-12.clj
+++ chapter2/exercise-2-12.clj
@@ -13,37 +13,49 @@
 ;; constructors: empty, push, pop
 ;; observers: top, empty?
 ;;
+;; The Stack interface contains two observers, as opposed to
+;; the Environment interface which only had one observer.
+;; The change here is that now the function passed back from
+;; each constructor must be able to distinguish between the 
+;; two observers and pass back the appropriate response.
+;;
 (ns eopl.ch02 (:use clojure.test))
 
 (defn empty
   {:doc "Returns an empty stack"}
   []
-  (fn [s]
-    ()))
+  (fn [func]
+    (cond
+      (= func 'empty?) true
+      :else (throw (IllegalArgumentException.)))))
 
 (defn push
   {:doc "Pushes a new element onto an existing stack"}
   [x s]
-  (fn [x]
-    x))
+  (fn [func]
+    (cond
+      (= func 'empty?) false
+      (= func 'top) x
+      (= func 'pop) s
+      :else (throw (IllegalArgumentException.)))))
 
 (defn empty?
   {:doc "Returns true if the given stack is empty, false otherwise"}
   [s]
-  (= s ()))
+  (s 'empty?))
 
 (defn pop
   {:doc "Removes the top element from a stack"}
   [s]
-  (fn [s] (rest s)))
+  (s 'pop))
 
 (defn top
   {:doc "Returns the top element of a stack"}
   [s]
-  (first s))
+  (s 'top))
 ;;
 ;; unit-tests
 ;;
 (def empt (empty))
-(is (= (empt ()) ()))
-(is (= (empt '(1 2)) ()))
+(is (= (empt 'empty?) true))
+(is (= (top (push 2 (push 1 (empty)))) 2))
