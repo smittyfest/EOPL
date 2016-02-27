7dc1e66c1bd281c69e884dbd1880fa546044b9fe
smittyfest <smittyfest@users.noreply.github.com>
2016-02-26 18:14:05 -0600

    Update exercise-2-15.clj

--- chapter2/exercise-2-15.clj
+++ chapter2/exercise-2-15.clj
@@ -4,4 +4,11 @@
 ;; @ Exercise 2.15[*] @
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
+;; Implement the Lambda Calculus expression interface
+;;
 (ns eopl.ch02 (:use clojure.test))
+
+(defn var-exp
+  {:doc "Identity function"}
+  [var]
+    (var))
