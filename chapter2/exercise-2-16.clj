1a5d144d6f9b595a2f0fbc6f4774967356f1d0ad
smittyfest <smittyfest@users.noreply.github.com>
2016-03-02 22:26:53 -0600

    Update exercise-2-16.clj

--- chapter2/exercise-2-16.clj
+++ chapter2/exercise-2-16.clj
@@ -5,3 +5,15 @@
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
 (ns eopl.ch02 (:use clojure.test))
+
+(defn var-exp
+  [var]
+    #(do %))
+
+(defn lambda-exp
+  [bound-var body]
+    (list 'lambda bound-var body))
+
+(defn app-exp
+  [exp1 exp2]
+    (list exp1 exp2))
