d4bfc21e3346d7fb5d4700a6e6cc524b2bd4b816
smittyfest <smittyfest@users.noreply.github.com>
2016-03-02 22:37:16 -0600

    Update exercise-2-16.clj

--- chapter2/exercise-2-16.clj
+++ chapter2/exercise-2-16.clj
@@ -17,3 +17,24 @@
 (defn app-exp
   [exp1 exp2]
     (list exp1 exp2))
+
+(defn var-exp?
+  {:doc "Returns true if the given expression is a variable expression, false otherwise"}
+  [exp]
+    (symbol? exp))
+
+(defn pair?
+  [exp]
+    (and (first exp) (second exp)))
+
+(defn lambda-exp?
+  [exp]
+    (and (pair? exp) (== 'lambda (first exp))))
+
+(defn app-exp?
+  [exp]
+    (and (pair? exp) (pair? (rest exp) (nil? (rest (rest exp))))))
+(defn var-exp->var
+  {:doc ""}
+  [exp]
+    (exp))
