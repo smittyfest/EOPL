32b92a52982ee2a5d2f72aed0411a6e3ea59ea45
smittyfest <smittyfest@users.noreply.github.com>
2016-03-02 22:36:10 -0600

    Update exercise-2-15.clj

--- chapter2/exercise-2-15.clj
+++ chapter2/exercise-2-15.clj
@@ -9,15 +9,33 @@
 (ns eopl.ch02 (:use clojure.test))
 
 (defn var-exp
-  {:doc "Identity function"}
   [var]
-    (var))
+    #(do %)) ;; var is a keyword in clojure, could pick a better parameter name but I like this identity function
+
+(defn lambda-exp
+  [bound-var body]
+    (list 'lambda bound-var body))
+
+(defn app-exp
+  [exp1 exp2]
+    (list exp1 exp2))
 
 (defn var-exp?
   {:doc "Returns true if the given expression is a variable expression, false otherwise"}
   [exp]
     (symbol? exp))
 
+(defn pair?
+  [exp]
+    (and (first exp) (second exp)))
+
+(defn lambda-exp?
+  [exp]
+    (and (pair? exp) (== 'lambda (first exp))))
+
+(defn app-exp?
+  [exp]
+    (and (pair? exp) (pair? (rest exp) (nil? (rest (rest exp))))))
 (defn var-exp->var
   {:doc ""}
   [exp]
