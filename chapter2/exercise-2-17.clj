7271b28e9b17ff485d94e94b21cb01c26c1c624a
smittyfest <smittyfest@users.noreply.github.com>
2016-03-12 11:45:09 -0600

    Update exercise-2-17.clj

--- chapter2/exercise-2-17.clj
+++ chapter2/exercise-2-17.clj
@@ -11,18 +11,19 @@
 (defn lambda-exp-a [bound-var body] (list 'lambda bound-var body))
 (defn app-exp-a [exp1 exp2] (list 'app exp1 exp2))
 
-(defn var-exp?-a [exp] (and (pair? exp) (eqv? 'var (car exp))))
-(defn lambda-exp?-a [exp] (and (pair? exp) (eqv? 'lambda (car exp))))
-(defn app-exp?-a [exp] (and (pair? exp) (eqv? 'app (car exp))))
+(defn pair? [exp] (and (first exp) (second exp)))
+(defn var-exp?-a [exp] (and (pair? exp) (= 'var (first exp))))
+(defn lambda-exp?-a [exp] (and (pair? exp) (= 'lambda (first exp))))
+(defn app-exp?-a [exp] (and (pair? exp) (= 'app (first exp))))
 
-(defn var-exp->var-a [exp] (cadr exp))
-(defn lambda-exp->bound-var-a [exp] (cadr exp))
-(defn lambda-exp->body-a [exp] (caddr exp))
-(defn app-exp->rator-a [exp] (cadr exp))
-(defn app-exp->rand-a [exp] (caddr exp))
+(defn var-exp->var-a [exp] (first (rest exp)))
+(defn lambda-exp->bound-var-a [exp] (first (rest exp)))
+(defn lambda-exp->body-a [exp] (first (rest (rest exp))))
+(defn app-exp->rator-a [exp] (first (rest exp)))
+(defn app-exp->rand-a [exp] (first (rest (rest exp))))
 
 ;; B) As Symbol/Vector Pairs:
-(defn var-exp-b [var] var)
+(defn var-exp-b [var] #(do %))
 (defn lambda-exp-b [bound-var body] (vector bound-var body))
 (defn app-exp-b [exp1 exp2] (cons exp1 exp2))
 
@@ -31,10 +32,10 @@
 (defn app-exp?-b [exp] (pair? exp))
 
 (defn var-exp->var-b [exp] exp)
-(defn lambda-exp->bound-var-b [exp] (vector-ref exp 0))
-(defn lambda-exp->body-b [exp] (vector-ref exp 1))
-(defn app-exp->rator-b [exp] (car exp))
-(defn app-exp->rand-b [exp] (cdr exp))
+(defn lambda-exp->bound-var-b [exp] (nth exp 0))
+(defn lambda-exp->body-b [exp] (nth exp 1))
+(defn app-exp->rator-b [exp] (first exp))
+(defn app-exp->rand-b [exp] (rest exp))
 ;;
 ;; unit-tests
 ;;
