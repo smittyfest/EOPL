9dea8b29f6e84a16439a2c49460aa740745bdc68
smittyfest <smittyfest@users.noreply.github.com>
2016-03-15 08:20:43 -0500

    Update exercise-2-22.clj

--- chapter2/exercise-2-22.clj
+++ chapter2/exercise-2-22.clj
@@ -4,4 +4,7 @@
 ;; @ Exercise 2.22[*] @
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
+;; Using "define-datatype" ("deftype" in Clojure),
+;; implement the Stack data type of exercise 2.4.
+;;
 (ns eopl.ch02 (:use clojure.test))
