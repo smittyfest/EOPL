cf4850244ec843560b9eac81a0c68caacdd88f9e
smittyfest <smittyfest@users.noreply.github.com>
2016-03-15 08:23:24 -0500

    Update exercise-2-23.clj

--- chapter2/exercise-2-23.clj
+++ chapter2/exercise-2-23.clj
@@ -4,4 +4,9 @@
 ;; @ Exercise 2.23[*] @
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
+;; The definition of lc-exp ignores the condition in definition 1.1.8
+;; that says "Identifier is any symbol other than lambda." Modify the definition of
+;; "identifier?" to capture this condition. As a hint, remember that any predicate
+;; can be used in "define-datatype" ("deftype" in Clojure), even ones you define.
+;;
 (ns eopl.ch02 (:use clojure.test))
