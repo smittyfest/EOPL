0c6358dd7cbaf7ce205f4994d45712e6a2c76177
smittyfest <smittyfest@users.noreply.github.com>
2016-03-14 19:09:51 -0500

    Update exercise-2-21.clj

--- chapter2/exercise-2-21.clj
+++ chapter2/exercise-2-21.clj
@@ -4,4 +4,7 @@
 ;; @ Exercise 2.21[*] @
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
+;; Implement the data type of environments, as in section 2.2.2, using
+;; "define-datatype" ("deftype" in Clojure). Then include "has-binding?" of exercise 2.9.
+;;
 (ns eopl.ch02 (:use clojure.test))
