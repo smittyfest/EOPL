c0dd1c64c71023a241b6148a2097991a54917caf
smittyfest <smittyfest@users.noreply.github.com>
2016-03-16 19:02:42 -0500

    Update exercise-2-22.clj

--- chapter2/exercise-2-22.clj
+++ chapter2/exercise-2-22.clj
@@ -15,3 +15,11 @@
   (push [this var s])
   (pop [this s])
   (top [this s]))
+
+(deftype Stack []
+  IStack
+  (empty-stack [this] ())
+  (empty-stack? [this s] (prn "tweet!"))
+  (push [this var s] (prn "woof!"))
+  (pop [this s] (prn "meow!"))
+  top [this s] (prn "ribbet!"))
