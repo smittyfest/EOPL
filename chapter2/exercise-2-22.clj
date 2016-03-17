47a829aa50a3e9108588a07130c24957b82bfdfc
smittyfest <smittyfest@users.noreply.github.com>
2016-03-16 19:00:29 -0500

    Update exercise-2-22.clj

--- chapter2/exercise-2-22.clj
+++ chapter2/exercise-2-22.clj
@@ -8,3 +8,10 @@
 ;; implement the Stack data type of exercise 2.4.
 ;;
 (ns eopl.ch02 (:use clojure.test))
+
+(defprotocol IStack
+  (empty-stack [this])
+  (empty-stack? [this s])
+  (push [this var s])
+  (pop [this s])
+  (top [this s]))
