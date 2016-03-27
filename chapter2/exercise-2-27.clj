de358f92b311f3b0261891c084a5215972c3c2c0
smittyfest <smittyfest@users.noreply.github.com>
2016-03-27 11:11:41 -0500

    Update exercise-2-27.clj

--- chapter2/exercise-2-27.clj
+++ chapter2/exercise-2-27.clj
@@ -5,3 +5,21 @@
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
 (ns eopl.ch02 (:use clojure.test))
+
+                 Lambda-Exp
+                /          \
+               /            \
+        Lambda-Exp        Var-Exp
+       /          \          |
+  Bound-Var       Body      var
+      |            |         |
+      a         App-Exp      c
+                /     \
+             rator    rand
+               |        |
+            Var-Exp  Var-Exp
+               |        |
+              var      var
+               |        |
+               a        b
+               
