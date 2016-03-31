754067019be490ba4f12b80fa2d710ec19d4de69
smittyfest <smittyfest@users.noreply.github.com>
2016-03-30 19:25:21 -0500

    Update exercise-2-28.clj

--- chapter2/exercise-2-28.clj
+++ chapter2/exercise-2-28.clj
@@ -4,4 +4,11 @@
 ;; @ Exercise 2.28[*] @
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
+;; Write an unparser that converts the abstract syntax
+;; of an LcExp into a String that matches the grammar:
+;;
+;; LcExp ::= Identifier [var-exp (var)]
+;;       ::= (lambda (Identifier) LcExp) [lambda-exp (bound-var body)]
+;;       ::= (LcExp LcExp) [app-exp (rator rand)]
+;;
 (ns eopl.ch02 (:use clojure.test))
