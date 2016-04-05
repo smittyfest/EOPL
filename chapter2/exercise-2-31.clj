745ccd0eab1d883273d5674faadd8dc0943598fd
smittyfest <smittyfest@users.noreply.github.com>
2016-04-04 19:55:02 -0500

    Update exercise-2-31.clj

--- chapter2/exercise-2-31.clj
+++ chapter2/exercise-2-31.clj
@@ -4,4 +4,7 @@
 ;; @ Exercise 2.31[**] @
 ;; @@@@@@@@@@@@@@@@@@@@@
 ;;
+;; Sometimes it is useful to specify a concrete syntax as a sequence
+;; of symbols and integers, surrounded by parentheses.
+;;
 (ns eopl.ch02 (:use clojure.test))
