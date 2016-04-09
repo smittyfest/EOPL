0d94ef51638fe4db157cd9ea43c52a7d4e058d46
smittyfest <smittyfest@users.noreply.github.com>
2016-04-09 15:40:43 -0500

    Update exercise-2-29.clj

--- chapter2/exercise-2-29.clj
+++ chapter2/exercise-2-29.clj
@@ -4,4 +4,20 @@
 ;; @ Exercise 2.29[*] @
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
+;; Where a Kleene Star or Kleene Plus is used in concrete syntax, it
+;; is most convenient to use a list of associated subtrees when constructing
+;; an abstract syntax tree. For example, if the grammar for lambda-calculus
+;; expressions had been:
+;;
+;; LcExp ::= Identifier
+;;           |var-exp (var)|
+;;       ::= (lambda ({Identifier}*) LcExp)
+;;           |lambda-exp (bound-vars body)|
+;;       ::= (LcExp {LcExp}*)
+;;           |app-exp (rator rands)|
+;;
+;; then the predicate for the bound-vars field could be (list-of identifier?),
+;; and the predicate for the rands field could be (list-of lc-exp?).
+;; Write a define-datatype and a parser for this grammar that works in this way.
+;;
 (ns eopl.ch02 (:use clojure.test))
