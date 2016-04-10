6ff6905acf7709946e53e8a470686e1b10cc5be2
smittyfest <smittyfest@users.noreply.github.com>
2016-04-10 14:00:15 -0500

    Update exercise-2-31.clj

--- chapter2/exercise-2-31.clj
+++ chapter2/exercise-2-31.clj
@@ -27,4 +27,17 @@
 ;; so that the example above produces the same abstract syntax tree as the sequence
 ;; of constructors:
 ;;
+;; (diff-exp
+;;   (diff-exp
+;;     (const-exp 3)
+;;     (const-exp 2))
+;;   (deff-exp
+;;     (const-exp 4)
+;;     (diff-exp
+;;       (const-exp 12)
+;;       (const-exp 7))))
+;;
+;; As a hint, consider writing a procedure that takes a list and produces a prefix-exp
+;; and the list of leftover list elements.
+;;
 (ns eopl.ch02 (:use clojure.test))
