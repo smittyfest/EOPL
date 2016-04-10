f13248c38e6f1a3691868f7bc7cb00f401afdc06
smittyfest <smittyfest@users.noreply.github.com>
2016-04-10 15:39:13 -0500

    Update exercise-2-31.clj

--- chapter2/exercise-2-31.clj
+++ chapter2/exercise-2-31.clj
@@ -120,13 +120,13 @@
 
 (deftest parse-prefix-test
   (is (= (parse-prefix '(- - 3 2 - 4 - 12 7))
-         '(diff-exp
-           (diff-exp
-            (const-exp 3)
-            (const-exp 2))
-           (diff-exp
-            (const-exp 4)
-            (diff-exp
-             (const-exp 12)
-             (const-exp 7)))))))
+    '(diff-exp
+      (diff-exp
+        (const-exp 3)
+        (const-exp 2))
+      (diff-exp
+        (const-exp 4)
+        (diff-exp
+         (const-exp 12)
+         (const-exp 7)))))))
 (run-tests)
