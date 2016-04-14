4bd47361c0fa8f6374d34646eb6a5c5e30475bcb
smittyfest <smittyfest@users.noreply.github.com>
2016-04-14 14:25:22 -0500

    Update exercise-2-04.clj

--- chapter2/exercise-2-04.clj
+++ chapter2/exercise-2-04.clj
@@ -4,6 +4,8 @@
 ;; @ Exercise 2.4[**] @
 ;; @@@@@@@@@@@@@@@@@@@@
 ;;
+;; Consider the data type of Stacks of values, with an interface consisting
+;; of the procedures 
 ;; (empty)       = ()
 ;; (push [f] s)  = (f . s)
 ;; (pop s)       = (cdr s)
